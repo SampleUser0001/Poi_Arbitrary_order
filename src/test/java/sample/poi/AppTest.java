68c232bde2177f160d108167465f60c82ea33960
DummyUser <DummyUser@anonymous.user>
2022-06-20 09:54:30 +0900

    Javaバージョン 11 -> 8 , mainClass指定

--- src/test/java/sample/poi/AppTest.java
+++ src/test/java/sample/poi/AppTest.java
@@ -9,15 +9,5 @@ import static org.hamcrest.Matchers.*;
 public class AppTest {
     
     private App app;
-    
-    @Before
-    public void setup() {
-        app = new App();
-    }
-    
-    @Test
-    public void test() {
-        String actual = app.execute();
-        assertThat(actual, is(equalTo("expected")));
-    }
+
 }
